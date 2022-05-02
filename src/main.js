2c1ccd185436e1a4ecaba3dc56b9646d92333d4d
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-05-01 21:07:33 -0700

    Removing manual import of primevue components as autoimporter handles this for us

--- src/main.js
+++ src/main.js
@@ -9,22 +9,9 @@ import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
 import PrimeVue from 'primevue/config';
-import InputText from 'primevue/inputtext';
-// import Button from 'primevue/button';
-// import Toast from 'primevue/toast';
-// import ToastService from 'primevue/toastservice';
-
 
 const app = createApp(App);
-// app.use(PrimeVue)
 app.use(PrimeVue);
-// app.use(ToastService);
-
-// app.component('InputText', InputText);
-// app.component('Button', Button);
-// app.component('Toast', Toast);
-
-
 app.use(store)
-.use(router)
-.mount("#app");
+app.use(router)
+app.mount("#app");
