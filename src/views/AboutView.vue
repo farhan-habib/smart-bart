d23015b38d652828bbab3eac6d73a24bc44402f3
Maya Cox <mayac2023@headroyce.org>
2022-05-13 09:10:39 -0700

    Adding generic info to aboutpage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,9 +2,11 @@
 </script>
 <template>
 	<div class="about">
-		<h1>This is an about page</h1>
+		<h1>SMART BART</h1>
+		<h>Smart Bart is a new tool for finding the most efficient route between two chosen stations </h>
 	</div>
 </template>
 
 <style lang="scss">
+
 </style>
