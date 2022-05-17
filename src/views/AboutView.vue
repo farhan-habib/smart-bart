71a57bce63521779a81f17c73f21ea39b3e7e753
Maya Cox <mayac2023@headroyce.org>
2022-05-17 12:31:37 -0700

    Home and About Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,37 +1,38 @@
 <script setup>
 import Accordion from 'primevue/accordion';
 import Button from 'primevue/button';
+import Fieldset from 'primevue/fieldset';
+
 
 </script>
 <template>
 	<h1>SMART BART</h1>
-	<div class="about">
-		<Accordion :multiple="true">
-			<AccordionTab header="About BART">
-				Bay Area Rapid Transit (BART) is a rapid transit system serving the San Francisco Bay Area in
-				California. BART serves 50 stations along six routes on 131 miles of rapid transit lines, including a
-				10mile spur line in eastern Contra Costa County which uses diesel multiple-unit trains and a 3.2 mile
-				automated guideway transit line to the Oakland International Airport. With an average of 411,000 weekday
-				passengers and 118 million annual passengers in fiscal year 2019, BART is the fifth-busiest heavy rail
-				rapid transit system in the United States and is operated by the Bay Area Rapid Transit District which
-				formed in 1957. The initial system opened in stages from 1972 to 1974. The system was extended most
-				recently on June 13, 2020, when Milpitas and Berryessa/North San José stations opened as part of the
-				Silicon Valley BART extension in partnership with the Santa Clara Valley Transportation Authority (VTA).
-				<br>
+	<Fieldset legend="About BART">
+		Bay Area Rapid Transit (BART) is a rapid transit system serving the San Francisco Bay Area in
+		California. BART serves 50 stations along six routes on 131 miles of rapid transit lines, including a
+		10mile spur line in eastern Contra Costa County which uses diesel multiple-unit trains and a 3.2 mile
+		automated guideway transit line to the Oakland International Airport. With an average of 411,000 weekday
+		passengers and 118 million annual passengers in fiscal year 2019, BART is the fifth-busiest heavy rail
+		rapid transit system in the United States and is operated by the Bay Area Rapid Transit District which
+		formed in 1957. The initial system opened in stages from 1972 to 1974. The system was extended most
+		recently on June 13, 2020, when Milpitas and Berryessa/North San José stations opened as part of the
+		Silicon Valley BART extension in partnership with the Santa Clara Valley Transportation Authority (VTA).
+		<br>
+		<Button label="Click here for more info" class="p-button-link" />
 
-				<Button label="Click here for more info" class="p-button-link" />
-			</AccordionTab>
-			<AccordionTab header="About SMART BART">
-				SMART BART is a new tool for finding the most efficient route between two chosen BART stations. Once you
-				input your starting and ending station, SMART BART will find the most efficent route in terms of times
-				and transfers between your chosen stations.<br>
-				<Button label="Click here to begin" class="p-button-link" />
-			</AccordionTab>
-			<AccordionTab header="More Info">
-				Content
-			</AccordionTab>
-		</Accordion>
-	</div>
+	</Fieldset>
+	<Fieldset legend="About SMART BART">
+		SMART BART is a new tool for finding the most efficient route between two chosen BART stations. Once you
+		input your starting and ending station, SMART BART will find the most efficent route in terms of times
+		and transfers between your chosen stations.<br>
+		<Button label="Click here to begin" class="p-button-link" />
+
+	</Fieldset>
+	<Accordion :multiple="true">
+		<AccordionTab header="More Info">
+			Content
+		</AccordionTab>
+	</Accordion>
 </template>
 
 <style lang="scss">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,61 +1,10 @@
 <script setup>
-import { ref } from "vue";
-
-const selectedBartStation = ref();
-const allBartStations = ref([
-	{ name: 'Australia', code: 'AU' },
-	{ name: 'Brazil', code: 'BR' },
-	{ name: 'China', code: 'CN' },
-	{ name: 'Egypt', code: 'EG' },
-	{ name: 'France', code: 'FR' },
-	{ name: 'Germany', code: 'DE' },
-	{ name: 'India', code: 'IN' },
-	{ name: 'Japan', code: 'JP' },
-	{ name: 'Spain', code: 'ES' },
-	{ name: 'United States', code: 'US' }
-]);
-
-function findRoute(){
-console.log("Home Route");
-console.log(selectedBartStation.value);
-}
-
 </script>
 
 <template>
+	<h1>Welcome to SMART BART!</h1>
 	<view>
-		<main>
-			{{ selectedBartStation }}
-			<Dropdown v-model="selectedBartStation" :options="allBartStations" optionLabel="name" :filter="true"
-				placeholder="Starting Station" :showClear="true">
-				
-				<template #value="bartStation">
-					<div class="country-item country-item-value" v-if="bartStation.value">
-						<!-- <img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" /> -->
-						<div>{{ bartStation.value.name }}</div>
-						<!-- the thing shown in the box when the item is selected -->
-					</div>
-					<span v-else>
-						<!-- The content shown when no option is selected, defined as placeholder property of DropDown element -->
-						{{ bartStation.placeholder }}
-					</span>
-				</template>
-				<template #option="slotProps">
-					<div class="country-item">
-						<!-- the thing shown in the actual dropdown menu -->
-						<!-- <img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" /> -->
-						<div>{{ slotProps.option.name }}</div>
-						
-					</div>
-				</template>
-			</Dropdown>
-
-		</main>
-		<router-link to="/dev">View Stations on Map</router-link>
-		<div>
-		<Button label="Search for the best route" @click="findRoute($event)"/>
-		</div>
-		<!-- <button type="submit">Find Best Route</button> -->
+		<router-link to="/findroute">View Stations on Map</router-link>
 	</view>
 
 </template>
@@ -64,22 +13,6 @@ console.log(selectedBartStation.value);
 main {
 	text-align: center;
 	margin-top: 5%;
-}
-
-button {
-	cursor: pointer;
-	margin-left: 50%
-}
-
-view {
-	align-content: center;
-}
-
-.p-dropdown {
-	width: 14rem;
-	&{	
-		text-align: left;
-	}
 }
 </style>
 
