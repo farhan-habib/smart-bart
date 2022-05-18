71e2d264dce7d905188d44290f3bd17257133a9b
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-05-17 22:58:47 -0700

    Revert "Adding info note telling user the bus icon represent stations"
    This reverts commit 7a55445567f0332c2cc3d11630e64aab37c2ea37.

--- src/components/bartMap.vue
+++ src/components/bartMap.vue
@@ -63,7 +63,7 @@ function drawPolyline(station1, station2, routeArray) {
 		opacity: 0.5,
 		weight: 8,
 		clickable: false
-	}).addTo(mapDiv);
+	}).addTo(mapDiv).bindPopup('best route.');;
 
 	mapDiv.setView(loc1, 12);
 }
