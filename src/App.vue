cd4141c85128e3798943292b09548f9c849f8477
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-05-18 05:56:16 -0700

    Removing development only pages so that project can be finalized and turned in

--- src/App.vue
+++ src/App.vue
@@ -13,47 +13,34 @@ userThemeSettings();
 
 // import darkMode from "@/styles/"
 let navBarItems = ref([
-  {
-    label: "Home",
-    icon: "pi pi-fw pi-home",
-    to: "/",
-  },
-  {
-    label: "About",
-    icon: "pi pi-fw pi-info-circle",
-    to: "/about",
-  },
-  {
-    label: "Settings",
-    icon: "pi pi-fw pi-cog",
-    to: "/settings",
-  },
-  {
-	label: "Find Route",
-	icon: "pi pi-fw pi-map",
-	to: "/findRoute",
-  },
-  {
-    seperator: true,
-  },
-  {
-    label: "Dev",
-    icon: "pi pi-fw pi-code",
-    to: "/dev",
-  },
-  {
-    label: "DevTwo",
-    icon: "pi pi-fw pi-code",
-    to: "/dev2",
-  },
+	{
+		label: "Home",
+		icon: "pi pi-fw pi-home",
+		to: "/",
+	},
+	{
+		label: "About",
+		icon: "pi pi-fw pi-info-circle",
+		to: "/about",
+	},
+	{
+		label: "Settings",
+		icon: "pi pi-fw pi-cog",
+		to: "/settings",
+	},
+	{
+		label: "Find Route",
+		icon: "pi pi-fw pi-map",
+		to: "/findRoute",
+	}
 ]);
 </script>
 
 <template>
-  <header>
-    <TabMenu v-model:model="navBarItems" />
-  </header>
-  <RouterView />
+	<header>
+		<TabMenu v-model:model="navBarItems" />
+	</header>
+	<RouterView />
 </template>
 
 <style lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+	createRouter,
+	createWebHistory
+} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
+	history: createWebHistory(
+		import.meta.env.BASE_URL),
+	routes: [{
 			path: "/",
 			name: "home",
 			component: HomeView,
@@ -29,23 +32,7 @@ const router = createRouter({
 			path: "/findRoute",
 			name: "FindRoute",
 			component: () => import("../views/FindRoute.vue"),
-		},
-		{
-			path: "/dev",
-			name: "Development 1",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import("../views/DevView.vue"),
-		},
-		{
-			path: "/dev2",
-			name: "Development 2",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import("../views/DevViewTwo.vue"),
-		},
+		}
 	],
 });
 
